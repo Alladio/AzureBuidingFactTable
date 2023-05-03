f2b07358b13e09c77b9cd313f39d5d5cf0b7cf68
Alladio Bonesso <Alladio@users.noreply.github.com>
2023-05-03 07:41:46 -0300

    switching partitions

--- Create table with partitions.sql
+++ Create table with partitions.sql
@@ -69,3 +69,19 @@ JOIN    sys.schemas    s    ON    t.[schema_id]   = s.[schema_id]
 JOIN    sys.indexes    i    ON    p.[object_id]   = i.[object_Id]
                             AND   p.[index_Id]    = i.[index_Id]
 WHERE t.[name] = 'logdata'
+
+
+SELECT o.name, pnp.index_id, pnp.partition_id, pnp.rows,   
+    pnp.data_compression_desc, pnp.pdw_node_id  
+FROM sys.pdw_nodes_partitions AS pnp  
+JOIN sys.pdw_nodes_tables AS NTables  
+    ON pnp.object_id = NTables.object_id  
+AND pnp.pdw_node_id = NTables.pdw_node_id  
+JOIN sys.pdw_table_mappings AS TMap  
+    ON NTables.name = TMap.physical_name 
+    AND substring(TMap.physical_name,40, 10) = pnp.distribution_id 
+JOIN sys.objects AS o  
+    ON TMap.object_id = o.object_id  
+WHERE o.name = 'logdata'  
+ORDER BY o.name, pnp.index_id, pnp.partition_id;  
+
